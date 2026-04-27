79f0216ce5e352e3332e9d3fa4bacf0d22a2a43f
MacArthur10 <belmacarthur100@gmail.com>
2026-04-27 14:40:00 +0100

    Remove unnecessary files

--- models/StatutEnseignant.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public enum StatutEnseignant {
-    TITULAIRE,
-    VACATAIRE,
-    ASSISTANT;
-}
--- models/TypeGroupe.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public enum TypeGroupe {
-    CM,
-    TD,
-    TP;
-}
